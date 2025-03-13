2114f3caa2d0e565fefe6fae93628c871e369e2c
Marius Johnsen Tjøstheim <marius.tjostheim@tietoevry.com>
2025-03-13 14:24:11 +0100

    Store

--- Provider/Program.cs
+++ Provider/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+using Provider;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSingleton<UserStore>();
 
 var app = builder.Build();
 
@@ -16,6 +19,13 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.MapGet("/users", (UserStore userStore) => userStore.GetUsers());
+app.MapPost("/users", (UserStore userStore, [FromBody]User user) => userStore.CreateUser(user));
+app.MapGet("/users/{id}", (UserStore userStore, int id) => userStore.GetUserById(id));
+app.MapPut("/users", (UserStore userStore, [FromBody]User user) => userStore.UpdateUser(user));
+app.MapDelete("/users/{id}", (UserStore userStore, int id) => userStore.DeleteUser(id));
+
 
 app.MapGet("/health", () => "OK").WithName("Health");
 
